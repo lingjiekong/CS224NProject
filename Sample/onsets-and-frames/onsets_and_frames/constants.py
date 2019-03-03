b8486d1ba4fd271d8d9f2c4f0fbe56fc153f5b83
yuanshenli <yuanshenli@ShenliMacbookProWired.stanford.edu>
2019-03-02 16:13:14 -0800

    Add multi-GPU support for mel.py, have NOT tested yet.

--- Sample/onsets-and-frames/onsets_and_frames/constants.py
+++ Sample/onsets-and-frames/onsets_and_frames/constants.py
@@ -15,4 +15,4 @@ MEL_FMIN = 30
 MEL_FMAX = SAMPLE_RATE // 2
 WINDOW_LENGTH = 2048
 
-DEFAULT_DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+DEFAULT_DEVICE = 'cuda:0' if torch.cuda.is_available() else 'cpu'
