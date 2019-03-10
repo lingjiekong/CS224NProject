17c0d6a361b583a7525350a3bcb704a35912f6be
lab <yuanshenlifml@gmail.com>
2019-03-09 17:27:52 -0800

    initial commit

--- Sample/onsets-and-frames/onsets_and_frames/transcriber.py
+++ Sample/onsets-and-frames/onsets_and_frames/transcriber.py
@@ -1,16 +1,10 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-from collections import namedtuple
-import sys
-from typing import List, Tuple, Dict, Set, Union
-import torch
-import torch.nn as nn
-import torch.nn.utils
 """
 A rough translation of Magenta's Onsets and Frames implementation [1].
+
     [1] https://github.com/tensorflow/magenta/blob/master/magenta/onsets_and_frames/onsets_frames_transcription/onsets_and_frames.py
 """
 
+import torch
 import torch.nn.functional as F
 from torch import nn
 
@@ -20,7 +14,8 @@ from .lstm import BiLSTM
 class ConvStack(nn.Module):
     def __init__(self, input_features, output_features):
         super().__init__()
-        
+
+        # input is batch_size * 1 channel * frames * input_features
         self.cnn = nn.Sequential(
             # layer 0
             nn.Conv2d(1, output_features // 16, (3, 3), padding=1),
@@ -35,47 +30,44 @@ class ConvStack(nn.Module):
             nn.Dropout(0.25),
             nn.Conv2d(output_features // 16, output_features // 8, (3, 3), padding=1),
             nn.BatchNorm2d(output_features // 8),
-            nn.ReLU()
+            nn.ReLU(),
         )
-        # add another conv2d layer to do highway 
-        self.conv2d = nn.Conv2d( output_features // 8, output_features // 8, (3, 3), padding=1)
-#        self.batchNorm= nn.BatchNorm2d(output_features // 8)
-#        self.relu = nn.ReLU()
-        self.projection = nn.Linear(in_features = input_features //2 , out_features = input_features //2 , bias= True)
-        self.gate_projection = nn.Linear(in_features = input_features // 2 , out_features = input_features //2, bias= True)
-       
-        self.maxPool =  nn.MaxPool2d((1, 2))
-        self.dropout = nn.Dropout(0.25)
         
+        self.cnn_dialation1 = nn.Sequential(
+             nn.Conv2d(output_features // 8, output_features // 8, (3, 3), padding=1, bias = True),
+             nn.ReLU(),
+             nn.Dropout(0.25),
+        )
+        
+                
+        self.cnn_dialation2 = nn.Sequential(
+             nn.Conv2d(output_features // 8, output_features // 8, (3, 3), padding=4,dilation=4, bias = True),
+             nn.ReLU(),
+             nn.Dropout(0.25),
+        )
+        
+        self.cnn_dialation3 = nn.Sequential(
+             nn.Conv2d(output_features // 8, output_features // 8, (3, 3), padding=8,dilation=8, bias = True),
+             nn.ReLU(),
+             nn.Dropout(0.25),
+        )
+        
+        self.post = nn.Sequential(
+            nn.MaxPool2d((1, 2)),
+            nn.Dropout(0.25),
+        )
         self.fc = nn.Sequential(
             nn.Linear((output_features // 8) * (input_features // 4), output_features),
             nn.Dropout(0.5)
         )
 
     def forward(self, mel):
-        
         x = mel.view(mel.size(0), 1, mel.size(1), mel.size(2))
-        
-        x_cnn = self.cnn(x)
-        
-        x_conv_out = self.conv2d(x_cnn)
-        
-        proj_hidden = self.projection(x_conv_out)
-        
-        x_proj = F.relu(proj_hidden)
-        
-        gate_hidden =  self.gate_projection(x_conv_out)
-        
-        x_gate = F.softmax(gate_hidden,3)
-        
-        x_highway = torch.mul(x_proj,x_gate) + torch.mul((1 - x_gate), x_conv_out)
-        
-        x_maxPool =  self.maxPool( x_highway )
-        x_drpout = self.dropout(x_maxPool)
- 
-   
-        x_t = x_drpout.transpose(1, 2).flatten(-2)
-        x = self.fc(x_t)
+        x = self.cnn(x)
+        dilation = self.cnn_dialation1(x) + self.cnn_dialation2(x) + self.cnn_dialation3(x)
+        x = self.post(dilation)
+        x = x.transpose(1, 2).flatten(-2)
+        x = self.fc(x)
         return x
 
 
@@ -153,3 +145,4 @@ class OnsetsAndFrames(nn.Module):
             return denominator
         else:
             return (onset_label * (velocity_label - velocity_pred) ** 2).sum() / denominator
+
